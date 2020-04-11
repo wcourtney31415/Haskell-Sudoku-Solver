6e12c24b9703d573f5e5734cb698f102bf1700ed
Wes Courtney <wcourtney31415@gmail.com>
2020-04-11 07:38:01 -0400

    Added several new unit tests regarding the ArraySubtraction function. Migrated those tests to their own file and began planning some personal standards for unit tests.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,13 +1,7 @@
-import Lib
 import Test.HUnit
+import TestArraySubtraction
 
 main :: IO Counts
 main = runTestTT tests
 
-tests = TestList [TestLabel "testArraySubtraction" testArraySubtraction]
-
-testArraySubtraction =
-  TestCase $ assertEqual
-  "Array Subtraction"
-  (arraySubtraction [1,2,3,4,5,6,7,8,9] [9,4])
-  [1,2,3,5,6,7,8]
+tests = TestList arraySubtractionTests
---
+++ test/TestArraySubtraction.hs
@@ -0,0 +1,50 @@
+module TestArraySubtraction where
+
+import Lib
+import Test.HUnit
+
+arraySubtractionTests = [
+  TestLabel "Array Subtraction" testArraySubtraction
+  , TestLabel
+    "Array Subtraction: empty target list"
+    testArraySubtractionBlankTargets
+  , TestLabel
+    "Array Subtraction: Empty target list, and duplicate elements."
+    arrSubBlankTargetsDupElem
+  , TestLabel
+    "Array Subtraction: Empty Source List."
+    arrSubBlankSourceArr
+  ]
+
+-- Tests --
+
+testArraySubtraction =
+  TestCase $ assertEqual
+  "Array Subtraction"
+  (arraySubtraction [1,2,3,4,5,6,7,8,9] [9,4])
+  [1,2,3,5,6,7,8]
+
+testArraySubtractionBlankTargets =
+  let
+    myList = [1,2,3,4,5,6,7,8,9]
+  in
+  TestCase $ assertEqual
+    "Empty target list"
+    (arraySubtraction myList [])
+    myList
+
+arrSubBlankTargetsDupElem =
+  let
+    myList = [1,2,3,4,5,6,7,8,9]
+    listWithDups = [1,2,3,4,5,6,7,8,9,2,4,6]
+  in
+  TestCase $ assertEqual
+    "Empty Target List"
+    (arraySubtraction myList [] /= listWithDups)
+    True
+
+arrSubBlankSourceArr =
+  TestCase $ assertEqual
+  "Empty Source List"
+  (arraySubtraction [] [4,6])
+  []
