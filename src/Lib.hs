deae3533ad2ac30f2f50ad8ccf3aea30a2558ddb
Wes Courtney <wcourtney31415@gmail.com>
2020-04-11 06:56:10 -0400

    Added a function for removing one array of elements from another array of elements. Created a unit test for it. removed some of the prewritten filler that comes with the project template.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,36 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Data.List
+
+arraySubtraction :: [Int] -> [Int] -> [Int]
+arraySubtraction array removalArray =
+  if removalArray == [] then
+    array
+  else
+    let
+      target = head removalArray
+      newArray = dropAllOccurances target array
+      updatedRemovalArray = tail removalArray
+    in
+      arraySubtraction newArray updatedRemovalArray
+
+dropAtIndex :: Int -> [Int] -> [Int]
+dropAtIndex index arr =
+  let
+    theSplit = splitAt index arr
+    front = fst theSplit
+    back =  tail $ snd theSplit
+  in
+    front ++ back
+
+dropAllOccurances :: Int -> [Int] -> [Int]
+dropAllOccurances val arr =
+  let
+    targets = findIndices (==val) arr
+    firstTarget = head targets
+    removedFirstOcc = dropAtIndex firstTarget arr
+  in
+    if targets == [] then
+      arr
+    else
+      dropAllOccurances val removedFirstOcc
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,8 +1,13 @@
+import Lib
 import Test.HUnit
 
 main :: IO Counts
 main = runTestTT tests
 
-testAddition = TestCase $ assertEqual "4+5=9" (4+5) 9
+tests = TestList [TestLabel "testArraySubtraction" testArraySubtraction]
 
-tests = TestList [TestLabel "testAddition" testAddition]
+testArraySubtraction =
+  TestCase $ assertEqual
+  "Array Subtraction"
+  (arraySubtraction [1,2,3,4,5,6,7,8,9] [9,4])
+  [1,2,3,5,6,7,8]
