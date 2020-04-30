fc3e66bbbb11c655db036a0416de69af61b5924c
wcourtney31415 <wcourtney31415@gmail.com>
2020-04-30 16:17:09 -0400

    Restart

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,10 +1,7 @@
 import Test.HUnit
-import TestArraySubtraction
 import TestPuzzle
 
 main :: IO Counts
 main = runTestTT tests
 
-tests = TestList $
-  arraySubtractionTests
-  ++ puzzleTests
+tests = TestList $ puzzleTests
--- test/TestPuzzle.hs
+++ test/TestPuzzle.hs
@@ -6,11 +6,7 @@ import CustomTestHelpers
 
 puzzleTests = prepTests "TestPuzzle.hs" tests
 
-tests = [
-  ("No Negative Cells", seedWithNegative)
-  , ("Invalid Row Count", incorrectNumOfRows)
-  , ("Invalid Cell Count", correctCellCount)
-  ]
+tests = []
 
 validPuzzleSeed =
   [
@@ -27,7 +23,7 @@ validPuzzleSeed =
     , [0,0,0, 0,8,0, 0,7,9]
   ]
 
-incorrectNumOfRows =
+makePuzzle =
   let
     invalidPuzzleSeed =
       [
@@ -38,54 +34,4 @@ incorrectNumOfRows =
         , [8,0,0, 0,6,0, 0,0,3]
       ]
   in
-  TestCase $ assertUnequal
-  "Invalid row count"
-  (toPuzzle invalidPuzzleSeed)
-  invalidPuzzleSeed
-
-seedWithNegative =
-  let
-    invalidPuzzleSeed =
-      [
-          [5,3,0, 0,-7,0, 0,0,0]
-        , [6,0,0, 1,9,5, 0,0,0]
-        , [0,9,8, 0,0,0, 0,6,0]
-        --
-        , [8,0,0, 0,6,0, 0,0,3]
-        , [4,0,0, 8,0,3, 0,-4,1]
-        , [7,0,0, 0,2,0, 0,0,6]
-        --
-        , [0,6,0, 0,0,0, 2,8,0]
-        , [0,-2,0, 4,1,9, 0,0,5]
-        , [0,0,0, 0,8,0, 0,7,9]
-      ]
-  in
-  TestCase $ assertUnequal
-  "No Negative Cells"
-  (toPuzzle invalidPuzzleSeed)
-  invalidPuzzleSeed
-
-
-
-correctCellCount =
-  let
-    invalidPuzzleSeed =
-      [
-        [5,3,0, 0,7,0, 0,0,0]
-        , [6,0,0, 1,9,5, 0,0,0]
-        , [0,9,8, 0,0,0, 0,6,0]
-        --
-        , [8,0,0, 0,6,0, 0,0,3]
-        , [4,0,0, 8,0,3, 0,0,1,5,5,5]
-        , [7,0,0, 0,2,0, 0,0,6]
-        --
-        , [0,6,0, 0,0,0, 2,8,0]
-        , [0,0,0, 4,1,9, 0,0,5]
-        , [0,0,0, 0,8,0, 0,7,9]
-        , [0,0,0, 0,8,0, 0,7,9]
-      ]
-  in
-  TestCase $ assertUnequal
-  "Incorrect Number of Cells"
-  (toPuzzle invalidPuzzleSeed)
-  invalidPuzzleSeed
+  TestCase $ assertFailure "bloop"
